3fc3f81cd4212df91372cd0dc462f274db5b5b00
ozefe <gh@efe.cv>
2024-10-05 18:54:33 +0300

    Add license, author and version information
    Signed-off-by: ozefe <gh@efe.cv>

--- bot.py
+++ bot.py
@@ -1,19 +1,40 @@
+# MIT License
+
+# Copyright (c) 2024 Seymapro
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from the_life import birthdate_to_life_path, life_path_to_content
 from paraphraser import paraphrase
 from telethon import TelegramClient, events, Button
 from datetime import datetime
 from pathlib import Path
 import os
-import re
 import json
 
+__author__ = "Seymapro"
+__version__ = "1.0.0"
 
 api_id = int(os.environ["OZETCIBOT_API_ID"])
 api_hash = os.environ["OZETCIBOT_API_HASH"]
 bot_token = os.environ["OZETCIBOT_BOT_TOKEN"]
 
-print("-" * 100)
-print(f"{api_id=}, {api_hash=}, {bot_token=}")
 
 client = TelegramClient("bot", api_id, api_hash).start(bot_token=bot_token)
 
